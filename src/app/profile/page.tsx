dfd99011b559f7a94982e86c3e0f46b1107b9d2b
Lymei <lymeicontato@gmail.com>
2023-06-06 13:01:40 -0300

    fix: fixed ghost import

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,6 @@ import { UserType } from '@/utils/types';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { supabaseClient } from '../layout';
-import { db } from '../api/users/route';
 import { users } from '@/db/schema';
 
 function Profile() {
